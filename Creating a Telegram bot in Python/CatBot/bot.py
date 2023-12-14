9b5d6e418c922855d5ba33f836e04662839531d6
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2023-12-14 12:41:56 +0200

    Update bot.py
    delete token

--- Creating a Telegram bot in Python/CatBot/bot.py
+++ Creating a Telegram bot in Python/CatBot/bot.py
@@ -2,7 +2,7 @@ import time
 from telegram import bot
 
 chat_id = 5361005636
-bot = bot.Bot("6390922177:AAFgiKcViZ5pwoLzDwL4QxqvSuzjVbtSj3A")
+bot = bot.Bot("YOUR_TOKEN")
 
 def send_random_cat():
     url = f"https://cataas.com/cat?t=${time.time()}&"
